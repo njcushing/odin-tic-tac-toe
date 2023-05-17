4ad1c687a507e7e330880d92e38538dea9ec6c4a
njcushing <129661299+njcushing@users.noreply.github.com>
2023-05-17 22:38:07 +0100

    Place gameBoard and displayControl objects in gameControl

--- index.js
+++ index.js
@@ -25,6 +25,156 @@ const gameControl = (() => {
     let _firstGameStarted = false;
     let _gameWin = false;
     let _gameDraw = false;
+    const _gameArea = document.querySelector(".game-area");
+    const _playerOneName = document.querySelector(".player-one .player-name");
+    const _playerTwoName = document.querySelector(".player-two .player-name");
+
+    _players.push(Player(null, "o"));
+    _players.push(Player(null, "x"));
+
+    const _changePlayerName = (e, index) => {
+        _players[index].setName(e.target.value);
+        if (_players[index].getName() === "") {
+            _players[index].setName(null);
+        }
+        _checkGamePlayable();
+    };
+    _playerOneName.addEventListener("input", () => _changePlayerName(event, 0));
+    _playerTwoName.addEventListener("input", () => _changePlayerName(event, 1));
+
+    const _checkGamePlayable = () => {
+        if (
+            _players[0].getName() !== null &&
+            (_players[1].getName() !== null || _AI)
+        ) {
+            displayControl.updateInformationString();
+            _firstGameStarted = true;
+            _gameArea.classList.add("playing");
+        } else {
+            displayControl.updateInformationString();
+            _gameArea.classList.remove("playing");
+        }
+    };
+
+    const reset = () => {
+        displayControl.resetGameCells();
+        gameBoard.reset();
+        _randomPlayer();
+        _movesPlayed = 0;
+        _gameWin = false;
+        _gameDraw = false;
+        _gameArea.classList.remove("game-over");
+        if (_firstGameStarted) {
+            displayControl.updateInformationString();
+        }
+    };
+
+    const _randomPlayer = () => {
+        turn = Math.floor(Math.random() * (_players.length - 1) + 0.5);
+    };
+
+    const changeBoardSize = () => {
+        let newBoardSize;
+        while (true) {
+            newBoardSize = prompt(
+                "Select a new board size between 3 and 9 inclusive:"
+            );
+            if (newBoardSize == null) {
+                return;
+            }
+            newBoardSize = parseInt(newBoardSize, 10);
+            if (Number.isNaN(newBoardSize)) {
+                continue;
+            }
+            if (newBoardSize < 3 || newBoardSize > 9) {
+                continue;
+            }
+            break;
+        }
+        gameBoard.setBoardSize(newBoardSize);
+        reset();
+    };
+
+    const place = (x, y) => {
+        if (!_gameArea.classList.contains("game-over")) {
+            const char = _players[turn].getChar();
+            if (gameBoard.place(x, y, char)) {
+                const cell = document.querySelector(
+                    `.game-cell[x="${x}"][y="${y}"]`
+                );
+                const img = cell.childNodes[0];
+                cell.classList.add(char);
+                switch (char) {
+                    case "o":
+                        img.setAttribute("src", "./img/nought.png");
+                        break;
+                    case "x":
+                        img.setAttribute("src", "./img/cross.png");
+                        break;
+                    default:
+                        break;
+                }
+                cell.childNodes[0].classList.add("no-select");
+                _movesPlayed++;
+                _checkWin(x, y, char);
+                turn = (turn + 1) % _players.length;
+                displayControl.updateInformationString();
+            }
+        }
+    };
+
+    const _checkWin = (x, y, char) => {
+        const boardState = gameBoard.getBoard();
+        const boardSize = gameBoard.getBoardSize();
+        for (let col = 0; col < boardState[y].length; col++) {
+            if (boardState[y][col] !== char) {
+                break;
+            }
+            if (col === boardState[y].length - 1) {
+                _gameWin = true;
+                _gameArea.classList.add("game-over");
+                return;
+            }
+        }
+        for (let row = 0; row < boardState.length; row++) {
+            if (boardState[row][x] !== char) {
+                break;
+            }
+            if (row === boardState.length - 1) {
+                _gameWin = true;
+                _gameArea.classList.add("game-over");
+                return;
+            }
+        }
+        if (x === y) {
+            for (let i = 0; i < boardState.length; i++) {
+                if (boardState[i][i] !== char) {
+                    break;
+                }
+                if (i === boardState.length - 1) {
+                    _gameWin = true;
+                    _gameArea.classList.add("game-over");
+                    return;
+                }
+            }
+        }
+        if (x + y === boardState.length - 1) {
+            for (let i = 0; i < boardState.length; i++) {
+                if (boardState[i][boardState.length - 1 - i] !== char) {
+                    break;
+                }
+                if (i === boardState.length - 1) {
+                    _gameWin = true;
+                    _gameArea.classList.add("game-over");
+                    return;
+                }
+            }
+        }
+        if (_movesPlayed === boardSize ** 2) {
+            _gameDraw = true;
+            _gameArea.classList.add("game-over");
+        }
+    };
 
     const gameBoard = (() => {
         let _boardSize = 3;
@@ -73,19 +223,15 @@ const gameControl = (() => {
         };
     })();
 
-    const displayControl = () => {
+    const displayControl = (() => {
         const _AIButton = document.querySelector(".ai-button");
         const _currentInfo = document.querySelector(".current-info");
-        const _gameArea = document.querySelector(".game-area");
         const _resetButton = document.querySelector(".reset-game");
         const _changeBoardSizeButton =
             document.querySelector(".change-board-size");
-        const _playerOneName = document.querySelector(
-            ".player-one .player-name"
-        );
-        const _playerTwoName = document.querySelector(
-            ".player-two .player-name"
-        );
+
+        _resetButton.addEventListener("click", reset);
+        _changeBoardSizeButton.addEventListener("click", changeBoardSize);
 
         const updateInformationString = () => {
             if (
@@ -186,156 +332,7 @@ const gameControl = (() => {
             updateInformationString,
             resetGameCells,
         };
-    };
-
-    _players.push(Player(null, "o"));
-    _players.push(Player(null, "x"));
-
-    const _changePlayerName = (e, index) => {
-        _players[index].setName(e.target.value);
-        if (_players[index].getName() === "") {
-            _players[index].setName(null);
-        }
-        _checkGamePlayable();
-    };
-    _playerOneName.addEventListener("input", () => _changePlayerName(event, 0));
-    _playerTwoName.addEventListener("input", () => _changePlayerName(event, 1));
-
-    const _checkGamePlayable = () => {
-        if (
-            _players[0].getName() !== null &&
-            (_players[1].getName() !== null || _AI)
-        ) {
-            displayControl.updateInformationString();
-            _firstGameStarted = true;
-            _gameArea.classList.add("playing");
-        } else {
-            displayControl.updateInformationString();
-            _gameArea.classList.remove("playing");
-        }
-    };
-
-    const reset = () => {
-        displayControl.resetGameCells();
-        gameBoard.reset();
-        _randomPlayer();
-        _movesPlayed = 0;
-        _gameWin = false;
-        _gameDraw = false;
-        _gameArea.classList.remove("game-over");
-        if (_firstGameStarted) {
-            displayControl.updateInformationString();
-        }
-    };
-    _resetButton.addEventListener("click", reset);
-
-    const _randomPlayer = () => {
-        turn = Math.floor(Math.random() * (_players.length - 1) + 0.5);
-    };
-
-    const _changeBoardSize = () => {
-        let newBoardSize;
-        while (true) {
-            newBoardSize = prompt(
-                "Select a new board size between 3 and 9 inclusive:"
-            );
-            if (newBoardSize == null) {
-                return;
-            }
-            newBoardSize = parseInt(newBoardSize, 10);
-            if (Number.isNaN(newBoardSize)) {
-                continue;
-            }
-            if (newBoardSize < 3 || newBoardSize > 9) {
-                continue;
-            }
-            break;
-        }
-        gameBoard.setBoardSize(newBoardSize);
-        reset();
-    };
-    _changeBoardSizeButton.addEventListener("click", _changeBoardSize);
-
-    const place = (x, y) => {
-        if (!_gameArea.classList.contains("game-over")) {
-            const char = _players[turn].getChar();
-            if (gameBoard.place(x, y, char)) {
-                const cell = document.querySelector(
-                    `.game-cell[x="${x}"][y="${y}"]`
-                );
-                const img = cell.childNodes[0];
-                cell.classList.add(char);
-                switch (char) {
-                    case "o":
-                        img.setAttribute("src", "./img/nought.png");
-                        break;
-                    case "x":
-                        img.setAttribute("src", "./img/cross.png");
-                        break;
-                    default:
-                        break;
-                }
-                cell.childNodes[0].classList.add("no-select");
-                _movesPlayed++;
-                _checkWin(x, y, char);
-                turn = (turn + 1) % _players.length;
-                displayControl.updateInformationString();
-            }
-        }
-    };
-
-    const _checkWin = (x, y, char) => {
-        const boardState = gameBoard.getBoard();
-        const boardSize = gameBoard.getBoardSize();
-        for (let col = 0; col < boardState[y].length; col++) {
-            if (boardState[y][col] !== char) {
-                break;
-            }
-            if (col === boardState[y].length - 1) {
-                _gameWin = true;
-                _gameArea.classList.add("game-over");
-                return;
-            }
-        }
-        for (let row = 0; row < boardState.length; row++) {
-            if (boardState[row][x] !== char) {
-                break;
-            }
-            if (row === boardState.length - 1) {
-                _gameWin = true;
-                _gameArea.classList.add("game-over");
-                return;
-            }
-        }
-        if (x === y) {
-            for (let i = 0; i < boardState.length; i++) {
-                if (boardState[i][i] !== char) {
-                    break;
-                }
-                if (i === boardState.length - 1) {
-                    _gameWin = true;
-                    _gameArea.classList.add("game-over");
-                    return;
-                }
-            }
-        }
-        if (x + y === boardState.length - 1) {
-            for (let i = 0; i < boardState.length; i++) {
-                if (boardState[i][boardState.length - 1 - i] !== char) {
-                    break;
-                }
-                if (i === boardState.length - 1) {
-                    _gameWin = true;
-                    _gameArea.classList.add("game-over");
-                    return;
-                }
-            }
-        }
-        if (_movesPlayed === boardSize ** 2) {
-            _gameDraw = true;
-            _gameArea.classList.add("game-over");
-        }
-    };
+    })();
 
     const getTurn = () => turn;
 
@@ -344,6 +341,7 @@ const gameControl = (() => {
     return {
         reset,
         place,
+        changeBoardSize,
         getTurn,
     };
 })();
